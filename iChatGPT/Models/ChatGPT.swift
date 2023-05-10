12f962b109fe1d4972d1e2c60f5342ea6a6127d0
tonyduan <tonyduan@tal.com>
2023-05-10 17:24:51 +0800

    update reverse proxy

--- iChatGPT/Models/ChatGPT.swift
+++ iChatGPT/Models/ChatGPT.swift
@@ -10,7 +10,7 @@ import Foundation
 import Combine
 import OpenAI
 
-let kDeafultAPIHost = "service-4mekgteu-1251187043.sg.apigw.tencentcs.com"
+let kDeafultAPIHost = "service-hby30r7o-1251187043.sg.apigw.tencentcs.com"
 let kDeafultAPITimeout = 30.0
 let kAPIModels = [Model.gpt3_5Turbo, Model.gpt4, Model.gpt4_32k, Model.gpt4_0314, Model.gpt4_32k_0314, Model.gpt3_5Turbo0301]
 
